b399b27fd78148d43ca40ff0d5f86006089a6d20
Omkar Rane <raneomkar32@gmail.com>
2023-12-04 16:04:31 +0530

    code updates

--- books_two.py
+++ books_two.py
@@ -10,26 +10,40 @@ class Book:
     author: str
     description: str
     rating: int
+    published_date: int
 
-    def __init__(self,id,title,author,description,rating):
+    def __init__(self,id,title,author,description,rating,published_date):
         self.id = id
         self.title = title
         self.author = author
         self.description = description
         self.rating = rating
+        self.published_date = published_date
 
 
 class  BookRequest(BaseModel):
-    id: Optional[int]
+    id: Optional[int] = Field(title='id is not needed')
     title: str = Field(min_length=3)
     author: str = Field(min_length=1)
     description: str = Field(min_length=1,max_length=100)
     rating: int = Field(gt=-1,lt=6)
+    published_date: int
+
+    class Config:
+        json_schema_extra = {
+           'example': {
+            'title':'A new book',
+            'author':'abc',
+            'description':'A new description for book',
+            'rating':5
+           }
+
+        }
 
 
 
         
-BOOKS = [Book(1,'CS','omkar','A nice book',6),Book(2,'CS','omkar','A nice book',5),Book(3,'CS','omkar','A nice book',3)]
+BOOKS = [Book(1,'CS','omkar','A nice book',6,2012),Book(2,'CS','omkar','A nice book',5,2013),Book(3,'CS','omkar','A nice book',7,2023)]
 
 @app.get("/books")
 async def read_all_books():
@@ -73,3 +87,28 @@ async def get_book_rating(book_rating: int):
     for book in BOOKS:
         if book.rating == book_rating:
             return book
+        
+
+@app.put('/books/update_book')
+async def update_book(book: BookRequest):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book.id:
+            BOOKS[i] = book
+
+
+@app.delete('/books/book_delete/{book_id}')
+async def book_delete(book_id: int):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book_id:
+            BOOKS.pop(i)
+            break
+
+@app.get('/books/search_by_year/{published_year}')
+async def find_out_book(published_year: int):
+    data = []
+    for book in BOOKS:
+        print(f"loop {book.published_date}")
+        print(f"Path param: {published_year}")
+        if book.published_date == published_year:
+            data.append(book)
+            return data
